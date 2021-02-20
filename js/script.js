3f482ae1c60910e5aa10be624aa9c2eda1ca3454
karla <karla@getharvest.com>
2021-02-20 09:50:17 -0800

    add pagination

--- js/script.js
+++ js/script.js
@@ -15,48 +15,52 @@ function showPage (list, page) {
   for (let i = 0; i < list.length; i++) {
     if (i >= startIndex & i < endIndex) {
       let studentInfo = `
-      <li class="student-item cf">
-      <div class="student-details">
-        <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
-        <h3>${list[i].name.first} ${list[i].name.last}</h3>
-        <span class="email">${list[i].email}</span>
-      </div>
-      <div class="joined-details">
-        <span class="date">Joined ${list[i].registered.date}</span>
-      </div>
-      </li>`;
+        <li class="student-item cf">
+        <div class="student-details">
+          <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+          <h3>${list[i].name.first} ${list[i].name.last}</h3>
+          <span class="email">${list[i].email}</span>
+        </div>
+        <div class="joined-details">
+          <span class="date">Joined ${list[i].registered.date}</span>
+        </div>
+        </li>`;
     ul.insertAdjacentHTML('beforeend', studentInfo);  
     }
   }
 }
 
-// FOR TESTING:
-// let list = [
-//   {
-//     name: {
-//       title: "Miss",
-//       first: "Ethel",
-//       last: "Dean",
-//     },
-//     email: "ethel.dean@example.com",
-//     registered: {
-//       date: "12-15-2005",
-//       age: 15,
-//     },
-//     picture: {
-//       large: "https://randomuser.me/api/portraits/women/25.jpg",
-//       medium: "https://randomuser.me/api/portraits/med/women/25.jpg",
-//       thumbnail: "https://randomuser.me/api/portraits/thumb/women/25.jpg",
-//     },
-//   }
-// ]
-// showPage(list, 1);
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
-
+function addPagination (array) {
+  let totalPages = array.length / 9;
+  let ul = document.getElementsByClassName('link-list')[0];
+  ul.innerHTML = '';
+  for (let i = 0; i < totalPages; i++) {
+    let pageButton = `
+      <li>
+        <button type="button">${i + 1}</button>
+      </li>`;
+    ul.insertAdjacentHTML('beforeend', pageButton);
+  }
+  ul.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+      let pages = ul.children;
+      let activePage = event.target; 
+      let pageNumber = activePage.textContent;
+      for (let i = 0; i < pages.length; i++) {
+        pages[i].className = '';
+      }
+      activePage.ClassName = 'active';
+      showPage(array, pageNumber);
+    } 
+  });
+}
 
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
